3411336de6c0b5936b75edbe6e17204e58890ab5
JingZhang Chen <iRoiocam@gmail.com>
2024-01-29 10:10:14 +0800

    license and dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,11 +3,9 @@ import sbt._
 object Library {
 
   // Versions
-  val bndVersion = "6.4.1"
   val specs2Version = "4.20.5"
 
   // Libraries
-  val bndLib = "biz.aQute.bnd" % "biz.aQute.bndlib" % bndVersion
   val specs2 = "org.specs2" %% "specs2-core" % specs2Version
 }
 
